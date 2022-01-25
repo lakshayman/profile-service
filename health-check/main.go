b9e2df9be15883240be3fd70d9fc9946a2831858
Lakshay Manchanda <lakshaymanchanda73@gmail.com>
2022-01-26 04:26:19 +0530

    abc

--- health-check/main.go
+++ health-check/main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"context"
-	"log"
+	"fmt"
+	// "context"
+	// "log"
 	"os"
 
-	firebase "firebase.google.com/go"
-	"google.golang.org/api/option"
+	// firebase "firebase.google.com/go"
+	// "google.golang.org/api/option"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -14,26 +15,26 @@ import (
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 
-	ctx := context.Background()
-	sa := option.WithCredentialsJSON([]byte(os.Getenv("firestoreCred")))
-	app, err := firebase.NewApp(ctx, nil, sa)
-	if err != nil {
-		log.Fatalln(err)
-	}
+	// ctx := context.Background()
+	// sa := option.WithCredentialsJSON([]byte(os.Getenv("firestoreCred")))
+	// app, err := firebase.NewApp(ctx, nil, sa)
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
 
-	client, err := app.Firestore(ctx)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	client.Collection("users").Add(ctx, map[string]interface{}{
-		"first": "Ada",
-		"last":  "Lovelace",
-		"born":  1815,
-	})
-	defer client.Close()
+	// client, err := app.Firestore(ctx)
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+	// client.Collection("users").Add(ctx, map[string]interface{}{
+	// 	"first": "Ada",
+	// 	"last":  "Lovelace",
+	// 	"born":  1815,
+	// })
+	// defer client.Close()
 
 	return events.APIGatewayProxyResponse{
-		Body:       "Connected",
+		Body:       fmt.Sprintf(os.Getenv("PipelineUserSecretKey-86CzG9I7ZMgZ")),
 		StatusCode: 200,
 	}, nil
 }
